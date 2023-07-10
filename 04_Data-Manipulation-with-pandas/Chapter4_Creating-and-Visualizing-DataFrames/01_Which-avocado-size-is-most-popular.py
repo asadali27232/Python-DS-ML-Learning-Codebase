c8dcc74e86ce5524d0ec1fe41b640a2c28f53312
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-10 23:14:56 +0500

    Updated

---
+++ 04_Data-Manipulation-with-pandas/Chapter4_Creating-and-Visualizing-DataFrames/01_Which-avocado-size-is-most-popular.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read 'homlessness.csv' into a DataFrame as homelessness
+avoplotto = pd.read_csv("04_Data-Manipulation-with-pandas\Chapter4_Creating-and-Visualizing-DataFrames\\avoplotto.csv", index_col=0, header=0)
