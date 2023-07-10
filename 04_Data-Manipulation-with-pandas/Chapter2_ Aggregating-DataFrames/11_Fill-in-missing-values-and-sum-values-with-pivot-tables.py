482a36b8cf7311a1899ffbf3d47b4dd35e6c943c
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-10 18:42:34 +0500

    Updated

--- 04_Data-Manipulation-with-pandas/Chapter2_ Aggregating-DataFrames/11_Fill-in-missing-values-and-sum-values-with-pivot-tables.py
+++ 04_Data-Manipulation-with-pandas/Chapter2_ Aggregating-DataFrames/11_Fill-in-missing-values-and-sum-values-with-pivot-tables.py
@@ -1,5 +1,10 @@
-import numpy as np
 import pandas as pd
 
 # Read 'homlessness.csv' into a DataFrame as homelessness
 sales = pd.read_csv("04_Data-Manipulation-with-pandas\Chapter2_ Aggregating-DataFrames\sales.csv", index_col=0, header=0)
+
+# Print mean weekly_sales by department and type; fill missing values with 0
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0))
+
+# Print the mean weekly_sales by department and type; fill missing values with 0s; sum all rows and cols
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", margins=True, fill_value=0))
