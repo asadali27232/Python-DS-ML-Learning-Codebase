653f890a405b43c1901b119e87032d053a00bc2b
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-10 17:35:00 +0500

    Updated

---
+++ 04_Data-Manipulation-with-pandas/Chapter2_ Aggregating-DataFrames/06_Counting-categorical-variables.py
@@ -0,0 +1,30 @@
+# Import pandas using the alias pd
+import pandas as pd
+
+# Read 'homlessness.csv' into a DataFrame as homelessness
+sales = pd.read_csv("04_Data-Manipulation-with-pandas\Chapter2_ Aggregating-DataFrames\sales.csv", index_col=0, header=0)
+
+
+# Drop duplicate store/type combinations
+store_types = sales.drop_duplicates(subset=["store", "type"])
+print(store_types.head())
+
+# Drop duplicate store/department combinations
+store_depts = sales.drop_duplicates(subset=["store", "department"])
+print(store_depts.head())
+
+# Count the number of stores of each type
+store_counts = store_types["type"].value_counts()
+print(store_counts)
+
+# Get the proportion of stores of each type
+store_props = store_types["type"].value_counts(normalize=True)
+print(store_props)
+
+# Count the number of each department number and sort
+dept_counts_sorted = store_depts["department"].value_counts(sort=True)
+print(dept_counts_sorted)
+
+# Get the proportion of departments of each number and sort
+dept_props_sorted = store_depts["department"].value_counts(sort=True, normalize=True)
+print(dept_props_sorted)
