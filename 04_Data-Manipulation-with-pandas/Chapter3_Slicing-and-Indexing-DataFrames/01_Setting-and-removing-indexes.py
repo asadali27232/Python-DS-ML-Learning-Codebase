7af79dcc2ce1e763c56c31768554c0bca1af61e5
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-10 19:33:21 +0500

    Updated

---
+++ 04_Data-Manipulation-with-pandas/Chapter3_Slicing-and-Indexing-DataFrames/01_Setting-and-removing-indexes.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Read 'homlessness.csv' into a DataFrame as homelessness
+temperatures = pd.read_csv("04_Data-Manipulation-with-pandas\Chapter3_Slicing-and-Indexing-DataFrames\\temperatures.csv", index_col=0, header=0)
+
+# Look at temperatures
+print(temperatures)
+
+# Set the index of temperatures to city
+temperatures_ind = temperatures.set_index("city")
+
+# Look at temperatures_ind
+print(temperatures_ind)
+
+# Reset the temperatures_ind index, keeping its contents
+print(temperatures_ind.reset_index())
+
+# Reset the temperatures_ind index, dropping its contents
+print(temperatures_ind.reset_index(drop=True))
