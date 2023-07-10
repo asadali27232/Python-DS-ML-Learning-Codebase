22870e2375a970d79d45b285586da2557632cea2
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-10 18:37:04 +0500

    Updated

---
+++ 04_Data-Manipulation-with-pandas/Chapter2_ Aggregating-DataFrames/10_Pivoting-on-one-variable.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+# Read 'homlessness.csv' into a DataFrame as homelessness
+sales = pd.read_csv("04_Data-Manipulation-with-pandas\Chapter2_ Aggregating-DataFrames\sales.csv", index_col=0, header=0)
+
+# Pivot for mean weekly_sales for each store type
+mean_sales_by_type = sales.pivot_table(values="weekly_sales", index="type")
+
+# Print mean_sales_by_type
+print(mean_sales_by_type)
+
+# Pivot for mean and median weekly_sales for each store type
+mean_med_sales_by_type = sales.pivot_table(values="weekly_sales", index="type", aggfunc=[np.mean, np.median])
+
+# Print mean_med_sales_by_type
+print(mean_med_sales_by_type)
+
+# Pivot for mean weekly_sales by store type and holiday
+mean_sales_by_type_holiday = sales.pivot_table(values="weekly_sales", index="type", columns="is_holiday")
+
+# Print mean_sales_by_type_holiday
+print(mean_sales_by_type_holiday)
---
+++ 04_Data-Manipulation-with-pandas/Chapter2_ Aggregating-DataFrames/11_Fill-in-missing-values-and-sum-values-with-pivot-tables.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+# Read 'homlessness.csv' into a DataFrame as homelessness
+sales = pd.read_csv("04_Data-Manipulation-with-pandas\Chapter2_ Aggregating-DataFrames\sales.csv", index_col=0, header=0)
