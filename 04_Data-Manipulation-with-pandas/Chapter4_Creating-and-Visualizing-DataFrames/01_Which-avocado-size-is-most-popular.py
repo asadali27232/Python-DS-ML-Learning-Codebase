278048cd7a95d88816ae189894ee494469d33a61
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-11 07:48:40 +0500

    Updated

--- 04_Data-Manipulation-with-pandas/Chapter4_Creating-and-Visualizing-DataFrames/01_Which-avocado-size-is-most-popular.py
+++ 04_Data-Manipulation-with-pandas/Chapter4_Creating-and-Visualizing-DataFrames/01_Which-avocado-size-is-most-popular.py
@@ -2,4 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read 'homlessness.csv' into a DataFrame as homelessness
-avoplotto = pd.read_csv("04_Data-Manipulation-with-pandas\Chapter4_Creating-and-Visualizing-DataFrames\\avoplotto.csv", index_col=0, header=0)
+avocados = pd.read_csv("04_Data-Manipulation-with-pandas\Chapter4_Creating-and-Visualizing-DataFrames\\avocados.csv", index_col=0, header=0)
+
+# Look at the first few rows of data
+print(avocados.head())
+
+# Get the total number of avocados sold of each size
+nb_sold_by_size = avocados.set_index("size").pivot_table(values="nb_sold", index="size", aggfunc=sum)
+print(nb_sold_by_size)
+
+# Create a bar plot of the number of avocados sold by size
+nb_sold_by_size.plot(kind="bar")
+
+# Show the plot
+plt.show()
