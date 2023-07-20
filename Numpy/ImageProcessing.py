f24abb42eac88a3be17f39822eedb9ba7b15b618
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-20 22:09:10 +0500

    Updated

--- Numpy/ImageProcessing.py
+++ Numpy/ImageProcessing.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Task 1: Reading and writing image in and from numpy array
-me = plt.imread("C:\\Users\\LENOVO\\Repos\\Python-DS-ML-Learning-Codebase\\Numpy\\images\\pic.jpeg")
+me = plt.imread("images/pic.jpeg")
 
 print(me.max())
 
 me_90 = np.rot90(me)
-plt.imsave("pic.png", me_90)
+plt.imsave("images/pic.png", me_90)
