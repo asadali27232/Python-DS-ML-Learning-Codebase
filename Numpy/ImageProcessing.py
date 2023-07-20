aae82f5c8bb36cd6024792a54686d91dd9f04fa0
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-20 22:20:00 +0500

    Updated

--- Numpy/ImageProcessing.py
+++ Numpy/ImageProcessing.py
@@ -1,10 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Task 1: Reading and writing image in and from numpy array
-me = plt.imread("images/pic.jpeg")
+# Task 0: Reading and writing image in and from numpy array
+my_iamge = plt.imread("images/pic.jpeg")
 
-print(me.max())
+# Image's pixel information
+print(my_iamge.min())
+print(my_iamge.max())
+print(np.average(my_iamge))
+print(my_iamge.shape)
+print(np.info(my_iamge))
 
-me_90 = np.rot90(me)
-plt.imsave("images/pic.png", me_90)
+my_iamge_rot90 = np.rot90(my_iamge)
+plt.imsave("images/my_iamge_rot90.png", my_iamge_rot90)
+my_iamge_T = np.transpose(my_iamge, (1, 0, 2))
+plt.imsave("images/my_iamge_T.png", my_iamge_T)
