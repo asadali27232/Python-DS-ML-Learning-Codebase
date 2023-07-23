cb09888dc7c8061ba932b62fdbf06f5cc5c21dee
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-23 22:06:19 +0500

    Updated

--- 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/02_Exploratory-Analysis-and-Summary-Statistics.py
+++ 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/02_Exploratory-Analysis-and-Summary-Statistics.py
@@ -22,3 +22,36 @@ retained = marketing[marketing["is_retained"] == True]["user_id"].nunique()
 # Calculate the retention rate
 retention_rate = retained / total_subscribers
 print(round(retention_rate * 100, 2), "%")
+
+# Isolate english speakers
+english_speakers = marketing[marketing["language_displayed"] == "English"]
+
+# Calculate the total number of English speaking users
+total = english_speakers["user_id"].nunique()
+
+# Calculate the number of English speakers who converted
+subscribers = english_speakers[english_speakers["converted"] == True]["user_id"].nunique()
+
+# Calculate conversion rate
+conversion_rate = subscribers / total
+print("English speaker conversion rate:", round(conversion_rate * 100, 2), "%")
+
+# Group by language_displayed and count unique users
+total = marketing.groupby("language_displayed")["user_id"].nunique()
+
+# Group by language_displayed and count unique conversions
+subscribers = marketing[marketing["converted"] == True].groupby("language_displayed")["user_id"].nunique()
+
+# Calculate the conversion rate for all languages
+language_conversion_rate = subscribers / total
+print(language_conversion_rate)
+
+# Group by date_served and count unique users
+total = marketing.groupby("date_served")["user_id"].nunique()
+
+# Group by date_served and count unique converted users
+subscribers = marketing[marketing["converted"] == True].groupby("date_served")["user_id"].nunique()
+
+# Calculate the conversion rate per day
+daily_conversion_rate = subscribers / total
+print(daily_conversion_rate)
