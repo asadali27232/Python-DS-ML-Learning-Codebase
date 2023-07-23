3316c5840440174d04b9dd9b7102806c59a2314b
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-23 20:35:38 +0500

    Updated

--- 05_Analyzing-Marketing-Campaigns-with-pandas/01_Pandas.py
+++ 05_Analyzing-Marketing-Campaigns-with-pandas/01_Pandas.py
@@ -12,3 +12,12 @@ print(marketing.describe())
 
 # Check column data types and non-missing values
 print(marketing.info())
+
+# Check the data type of is_retained
+print(marketing["is_retained"].dtype)
+
+# Convert is_retained to a boolean
+marketing["is_retained"] = marketing["is_retained"].astype("bool")
+
+# Check the data type of is_retained, again
+print(marketing["is_retained"].dtype)
