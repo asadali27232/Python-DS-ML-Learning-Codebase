e66d876e8a7c352cc7f4fa8f9167e980ccfa7f6c
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-23 20:50:15 +0500

    Updated

--- 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/01_Pandas.py
+++ 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/01_Pandas.py
@@ -27,3 +27,9 @@ channel_dict = {"House Ads": 1, "Instagram": 2, "Facebook": 3, "Email": 4, "Push
 
 # Map the channel to a channel code
 marketing["channel_code"] = marketing["subscribing_channel"].map(channel_dict)
+
+# Import numpy
+import numpy as np
+
+# Add the new column is_correct_lang
+marketing["is_correct_lang"] = np.where(marketing["language_preferred"] == marketing["language_displayed"], "Yes", "No")
