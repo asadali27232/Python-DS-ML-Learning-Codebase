a679b29050b89282b1a21b33e97eb6cb15f18bec
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-23 20:44:49 +0500

    Updated

--- 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/01_Pandas.py
+++ 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/01_Pandas.py
@@ -21,3 +21,9 @@ marketing["is_retained"] = marketing["is_retained"].astype("bool")
 
 # Check the data type of is_retained, again
 print(marketing["is_retained"].dtype)
+
+# Mapping for channels
+channel_dict = {"House Ads": 1, "Instagram": 2, "Facebook": 3, "Email": 4, "Push": 5}
+
+# Map the channel to a channel code
+marketing["channel_code"] = marketing["subscribing_channel"].map(channel_dict)
