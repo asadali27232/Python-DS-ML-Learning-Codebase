98ded9bd497a03deb0a65e9a97bd69d59dc78d9b
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-27 23:48:44 +0500

    Updated

--- 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/02_Exploratory-Analysis-and-Summary-Statistics.py
+++ 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/02_Exploratory-Analysis-and-Summary-Statistics.py
@@ -90,7 +90,7 @@ plt.title("Daily conversion rate\n", size=16)
 plt.ylabel("Conversion rate (%)", size=14)
 plt.xlabel("Date", size=14)
 
-# Set the y-axis to begin at 0
+#Set the y-axis to begin at 0
 plt.ylim(0)
 
 # Display the plots
