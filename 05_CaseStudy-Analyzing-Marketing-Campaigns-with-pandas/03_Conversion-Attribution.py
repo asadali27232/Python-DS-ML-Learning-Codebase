e51a3f31f0a2ccf85f6c03e162f8b1dccc04a3fe
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-29 20:02:42 +0500

    Updated

---
+++ 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/03_Conversion-Attribution.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+marketing = pd.read_csv("marketing.csv", parse_dates=["date_served", "date_subscribed", "date_canceled"])
+
+def conversion_rate(dataframe, column_names):
+    # Total number of converted users
+    column_conv = dataframe[dataframe["converted"] == True].groupby(column_names)["user_id"].nunique()
+
+    # Total number users
+    column_total = dataframe.groupby(column_names)["user_id"].nunique()
+
+    # Conversion rate
+    conversion_rate = column_conv / column_total
+
+    # Fill missing values with 0
+    conversion_rate = conversion_rate.fillna(0)
+    return conversion_rate
+
+
+# Calculate conversion rate by age_group
+age_group_conv = conversion_rate(marketing, ["date_served", "age_group"])
+print(age_group_conv)
+
+# Unstack and create a DataFrame
+age_group_df = pd.DataFrame(age_group_conv.unstack(level=1))
+
+# Visualize conversion by age_group
+age_group_df.plot()
+plt.title("Conversion rate by age group\n", size=16)
+plt.ylabel("Conversion rate", size=14)
+plt.xlabel("Age group", size=14)
+plt.show()
+
