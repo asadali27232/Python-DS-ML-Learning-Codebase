58d3c407ffd0701c5c6842b02edbcaf113a15413
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-29 20:07:14 +0500

    Updated

--- 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/03_Conversion-Attribution.py
+++ 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/03_Conversion-Attribution.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 marketing = pd.read_csv("marketing.csv", parse_dates=["date_served", "date_subscribed", "date_canceled"])
 
+
 def conversion_rate(dataframe, column_names):
     # Total number of converted users
     column_conv = dataframe[dataframe["converted"] == True].groupby(column_names)["user_id"].nunique()
@@ -32,3 +33,24 @@ plt.ylabel("Conversion rate", size=14)
 plt.xlabel("Age group", size=14)
 plt.show()
 
+
+def plotting_conv(dataframe):
+    for column in dataframe:
+        # Plot column by dataframe's index
+        plt.plot(dataframe.index, dataframe[column])
+        plt.title("Daily " + str(column) + " conversion rate\n", size=16)
+        plt.ylabel("Conversion rate", size=14)
+        plt.xlabel("Date", size=14)
+        # Show plot
+        plt.show()
+        plt.clf()
+
+
+# Calculate conversion rate by date served and age group
+age_group_conv = conversion_rate(marketing, ["date_served", "age_group"])
+
+# Unstack age_group_conv and create a DataFrame
+age_group_df = pd.DataFrame(age_group_conv.unstack(level=1))
+
+# Plot the results
+plotting_conv(age_group_df)
