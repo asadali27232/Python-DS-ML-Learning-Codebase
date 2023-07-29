176b3c2f9b3704bc5da048279739ddb35c30e7b3
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-29 19:45:43 +0500

    Updated

--- 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/02_Exploratory-Analysis-and-Summary-Statistics.py
+++ 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/02_Exploratory-Analysis-and-Summary-Statistics.py
@@ -118,7 +118,21 @@ retention_total = marketing.groupby(["date_subscribed", "subscribing_channel"])[
 print(retention_total.head())
 
 # Sum the retained subs by subscribing channel and date subscribed
-retention_subs = marketing[marketing['is_retained'] == True].groupby(['date_subscribed', 'subscribing_channel'])['user_id'].nunique()
+retention_subs = marketing[marketing["is_retained"] == True].groupby(["date_subscribed", "subscribing_channel"])["user_id"].nunique()
 
 # Print results
 print(retention_subs.head())
+
+# Divide retained subscribers by total subscribers
+retention_rate = retention_subs / retention_total
+retention_rate_df = pd.DataFrame(retention_rate.unstack(level=1))
+
+# Plot retention rate
+retention_rate_df.plot()
+
+# Add a title, x-label, y-label, legend and display the plot
+plt.title("Retention Rate by Subscribing Channel")
+plt.xlabel("Date Subscribed")
+plt.ylabel("Retention Rate (%)")
+plt.legend(loc="upper right", labels=retention_rate_df.columns.values)
+plt.show()
