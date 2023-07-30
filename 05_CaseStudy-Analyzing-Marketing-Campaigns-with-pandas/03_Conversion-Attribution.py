364159988e18940b635003b42b480cc7a0c4f536
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-30 09:13:23 +0500

    Updated

--- 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/03_Conversion-Attribution.py
+++ 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/03_Conversion-Attribution.py
@@ -105,3 +105,10 @@ language_check_df = pd.DataFrame(language_check.unstack(level=1)).fillna(0)
 
 # Print results
 print(language_check_df)
+
+# Divide the count where language is correct by the row sum
+language_check_df["pct"] = language_check_df["Yes"] / language_check_df.sum(axis=1)
+
+# Plot and show your results
+plt.plot(language_check_df.index.values, language_check_df["pct"])
+plt.show()
