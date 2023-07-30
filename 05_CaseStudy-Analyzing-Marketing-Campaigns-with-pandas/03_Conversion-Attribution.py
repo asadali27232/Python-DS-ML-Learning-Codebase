9bb58688aede9e38e150707cf0d3b3ee8a235663
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-30 09:28:44 +0500

    Updated

--- 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/03_Conversion-Attribution.py
+++ 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/03_Conversion-Attribution.py
@@ -112,3 +112,48 @@ language_check_df["pct"] = language_check_df["Yes"] / language_check_df.sum(axis
 # Plot and show your results
 plt.plot(language_check_df.index.values, language_check_df["pct"])
 plt.show()
+
+# Calculate pre-error conversion rate
+house_ads_bug = house_ads[house_ads["date_served"] < "2018-01-11"]
+lang_conv = conversion_rate(house_ads_bug, "language_displayed")
+
+# Index other language conversion rate against English
+spanish_index = lang_conv["Spanish"] / lang_conv["English"]
+arabic_index = lang_conv["Arabic"] / lang_conv["English"]
+german_index = lang_conv["German"] / lang_conv["English"]
+
+print("Spanish index:", spanish_index)
+print("Arabic index:", arabic_index)
+print("German index:", german_index)
+
+# Group house_ads by date and language
+converted = house_ads.groupby(["date_served", "language_preferred"]).agg({"user_id": "nunique", "converted": "sum"})
+
+# Unstack converted
+converted_df = pd.DataFrame(converted.unstack(level=1))
+
+# Create English conversion rate column for affected period
+converted["english_conv_rate"] = converted.loc["2018-01-11":"2018-01-31"][("converted", "English")]
+
+# Create expected conversion rates for each language
+converted["expected_spanish_rate"] = converted["english_conv_rate"] * spanish_index
+converted["expected_arabic_rate"] = converted["english_conv_rate"] * arabic_index
+converted["expected_german_rate"] = converted["english_conv_rate"] * german_index
+
+# Multiply number of users by the expected conversion rate
+converted["expected_spanish_conv"] = converted["expected_spanish_rate"] / 100 * converted[("user_id", "Spanish")]
+converted["expected_arabic_conv"] = converted["expected_arabic_rate"] / 100 * converted[("user_id", "Arabic")]
+converted["expected_german_conv"] = converted["expected_german_rate"] / 100 * converted[("user_id", "German")]
+
+# Use .loc to slice only the relevant dates
+converted = converted.loc["2018-01-11":"2018-01-31"]
+
+# Sum expected subscribers for each language
+expected_subs = converted["expected_spanish_conv"].sum() + converted["expected_arabic_conv"].sum() + converted["expected_german_conv"].sum()
+
+# Calculate how many subscribers we actually got
+actual_subs = converted[("converted", "Spanish")].sum() + converted[("converted", "Arabic")].sum() + converted[("converted", "German")].sum()
+
+# Subtract how many subscribers we got despite the bug
+lost_subs = expected_subs - actual_subs
+print(lost_subs)
