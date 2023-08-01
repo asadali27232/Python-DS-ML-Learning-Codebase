e6a97d3218a44ddb834f9d63b4b704996117e3de
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-08-01 16:53:52 +0500

    Updated

--- 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/04_Personalization-AB-Test.py
+++ 05_CaseStudy-Analyzing-Marketing-Campaigns-with-pandas/04_Personalization-AB-Test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
-ffff 
+fffff = pd.read_csv("fffff.csv")
 marketing = pd.read_csv("marketing.csv", parse_dates=["date_served", "date_subscribed", "date_canceled"])
 
 # Subset the DataFrame
