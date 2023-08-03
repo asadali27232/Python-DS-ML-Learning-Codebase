706c8ce53461ec3816c67dc9930617c57bd9a278
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-08-03 14:46:24 +0500

    Updated

--- 06_Joining-Data-with-pandas/Chapter1_Data-Merging-Basics/01_Your-first-inner-join.py
+++ 06_Joining-Data-with-pandas/Chapter1_Data-Merging-Basics/01_Your-first-inner-join.py
@@ -16,7 +16,7 @@ taxi_own_veh = taxi_owners.merge(taxi_veh, on="vid", suffixes=["_own", "_veh"])
 print(taxi_own_veh.columns)
 
 # Merge the taxi_owners and taxi_veh tables setting a suffix
-taxi_own_veh = taxi_owners.merge(taxi_veh, on='vid', suffixes=('_own','_veh'))
+taxi_own_veh = taxi_owners.merge(taxi_veh, on="vid", suffixes=("_own", "_veh"))
 
 # Print the value_counts to find the most popular fuel_type
-print(taxi_own_veh['fuel_type'].value_counts())
+print(taxi_own_veh["fuel_type"].value_counts())
