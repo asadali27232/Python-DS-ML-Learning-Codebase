3ab0078dc799014cbb20f15842cfc6df0d88be69
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-08-05 16:43:33 +0500

    Updated

--- 06_Joining-Data-with-pandas/Chapter1_Data-Merging-Basics/converter.py
+++ 06_Joining-Data-with-pandas/Chapter1_Data-Merging-Basics/converter.py
@@ -1,14 +1,8 @@
 import pickle as pkl
 import pandas as pd
 
-with open("licenses.p", "rb") as f:
+with open("stations.p", "rb") as f:
     object = pkl.load(f)
 
 df = pd.DataFrame(object)
-df.to_csv(r"licenses.csv")
-
-with open("business_owners.p", "rb") as f:
-    object = pkl.load(f)
-
-df = pd.DataFrame(object)
-df.to_csv(r"biz_owners.csv")
+df.to_csv(r"stations.csv")
