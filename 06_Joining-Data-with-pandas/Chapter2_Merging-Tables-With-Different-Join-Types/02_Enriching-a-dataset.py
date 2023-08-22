dc17a1bb54b60884758957e95123f894c1c7a48c
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-08-22 09:26:53 +0500

    Updated

--- 06_Joining-Data-with-pandas/Chapter2_Merging-Tables-With-Different-Join-Types/02_Enriching-a-dataset.py
+++ 06_Joining-Data-with-pandas/Chapter2_Merging-Tables-With-Different-Join-Types/02_Enriching-a-dataset.py
@@ -1,5 +1,17 @@
+import pandas as pd
+
+taglines = pd.read_csv("taglines.csv")
+toy_story = pd.read_csv("toy_story.csv")
+
 # Merge the toy_story and taglines tables with a left join
 toystory_tag = toy_story.merge(taglines, on="id", how="left")
+
+# Print the rows and shape of toystory_tag
+print(toystory_tag)
+print(toystory_tag.shape)
+
+# Merge the toy_story and taglines tables with a inner join
+toystory_tag = toy_story.merge(taglines, on="id")
 
 # Print the rows and shape of toystory_tag
 print(toystory_tag)
