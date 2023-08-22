32b266be82aabba7cbe043fca848c40e436cd5f2
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-08-22 09:06:15 +0500

    Updated

--- 06_Joining-Data-with-pandas/Chapter2_Merging-Tables-With-Different-Join-Types/01_Counting-missing-rows-with-left-join.py
+++ 06_Joining-Data-with-pandas/Chapter2_Merging-Tables-With-Different-Join-Types/01_Counting-missing-rows-with-left-join.py
@@ -4,6 +4,10 @@ movies = pd.read_csv("movies.csv")
 financials = pd.read_csv("financials.csv")
 
 # Merge movies and financials with a left join
-movies_financials = movies.merge(financials, how="left")
+movies_financials = movies.merge(financials, on="id", how="left")
 
-print(movies_financials.head())
+# Count the number of rows in the budget column that are missing
+number_of_missing_fin = movies_financials['budget'].isnull().sum()
+
+# Print the number of movies missing financials
+print(number_of_missing_fin)
